f0554503b0dff934ec647eaee4bd9d4727bb6305
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-09-27 18:53:37 -0400

    Test POST /tasks

--- test/api.test.js
+++ test/api.test.js
@@ -1,6 +1,10 @@
-const server = require('../lib/api').server
+const fs = require('fs')
+const path = require('path')
 const request = require('supertest')
+const server = require('../lib/api').server
 const test = require('tape')
+
+var unconnected = path.join(__dirname, 'fixtures/unconnected-highways.json')
 
 test('GET /', function (t) {
   request(server)
@@ -9,6 +13,18 @@ test('GET /', function (t) {
   .expect(200)
   .end(function (err, res) {
     t.deepEqual(res.body, [], 'empty array')
+    t.end()
+  })
+})
+
+test('POST /tasks', function (t) {
+  request(server)
+  .post('/tasks')
+  .send(fs.readFileSync(unconnected).toString())
+  .expect(200)
+  .end(function (err, res) {
+    t.error(err, 'no error')
+    t.deepEqual(res.body, 'received 5573 tasks', 'returns count')
     t.end()
   })
 })
